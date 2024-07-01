f1dc1bc00d28f98c159fe4a3f3a16d3331ba6619
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-07-01 02:20:07 -0500

    Slaider movil

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const izquierda = document.getElementById("izquierda"),
-        derecha = document.getElementById("derecha");
+document.addEventListener("DOMContentLoaded", function () {
+  const slaider = document.getElementById("slaider"),
+    itemSlaider = document.querySelectorAll(".itemSlaider"),
+    izquierda = document.getElementById("izquierda"),
+    derecha = document.getElementById("derecha");
 
 
-derecha.addEventListener("click", ()=>{
-    console.log("object");
-})
+  let inicio = 0;
+  const itemSlaiderAncho = itemSlaider[0].offsetWidth;
+  const intervalo = 2000;
+
+
+  function siguienteSlide() {
+    inicio++;
+    if (inicio >= itemSlaider.length) {
+      inicio = 0;
+    }
+    moverCarrusel();
+  }
+
+  function anteriorSlide() {
+    inicio--;
+    if (inicio < 0) {
+      inicio = itemSlaider.length - 1;
+    }
+    moverCarrusel();
+  }
+
+  function moverCarrusel() {
+    const posicion = -inicio * itemSlaiderAncho;
+    slaider.style.transition = 'transform 0.8s ease';
+    slaider.style.transform = `translateX(${posicion}px)`;
+  }
+
+  setInterval(siguienteSlide, intervalo); 
+  moverCarrusel();
+  
+  derecha.addEventListener('click', siguienteSlide);
+  izquierda.addEventListener('click', anteriorSlide);
+
+});
