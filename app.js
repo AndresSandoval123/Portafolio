eca1fdb7a27b8bd5e2739e8df014b16f0f3f8b44
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-07-27 23:19:31 -0500

    mejoras

--- app.js
+++ app.js
@@ -1,36 +1,106 @@
-let currentIndex = 0; /* se define un indice de inicio */
-function navegar(direccion){
-    const slaider = document.getElementById("slaider");
-    const itemSlaider = document.querySelectorAll(".itemSlaider");
-    
-    currentIndex = (currentIndex + direccion + itemSlaider.length) % itemSlaider.length;
-    const offset = currentIndex * -8;
-    
-    slaider.style.transform = `translateX(${offset}%)`;
-    
-    console.log(offset);
+/* Seccion sobre mi / Frases */
+document.addEventListener('DOMContentLoaded', function() {
+  const frase = document.querySelectorAll('.frase');
+  let i = 0;
+
+  function mostrarSiguiente() {
+    frase[i].classList.remove('active'); // elimina la clase y oculta la frase
+    i = (i + 1) % frase.length; // Avanza a la siguiente frase
+    frase[i].classList.add('active'); // Muestra la siguiente frase
+  }
+
+  // Inicializa la primera frase como visible
+  frase[i].classList.add('active');
+
+  // Cambia la frase cada 2 segundos
+  setInterval(mostrarSiguiente, 2000);
+}); 
+
+/* Seccion tecnologias */
+const root = document.documentElement;
+const slaiderElementoDisplayed = getComputedStyle(root).getPropertyValue("--slaider-elemento-displayed");
+const slaider = document.querySelector("ul.slaider");
+
+root.style.setProperty("--slaider-elemento", slaider.children.length);
+
+for (let i = 0; i < slaiderElementoDisplayed; i++) {
+  slaider.appendChild(slaider.children[i].cloneNode(true));
 }
 
-/* Se realiza eventos a los botones y se le da la funcion navegar */
-document.getElementById("izquierda").addEventListener("click", ()=> {
-    navegar(-1);
-});
+/* Seccion proyectos */
 
-document.getElementById("derecha").addEventListener("click", ()=> {
-    navegar(1);
-});
-/* Se incia en 0 para generar el desplazamiento automatico */
-let automatico = 0;
+let imagenes = [
+  {
+    url: 'https://i.postimg.cc/4x6y0ntX/jampa.png',
+    nombre: 'E-commerce',
+    descripcion: 'Plataforma de comercio electrónico llamada "JAMPA shop".'
+  },
+  {
+    url: 'https://i.postimg.cc/LsSP3D2T/Wireframespng.png',
+    nombre: 'Wireframes',
+    descripcion: 'Creados en Figma para diseñar interfaces y prototipos.'
+  },
+  {
+    url: 'https://i.postimg.cc/SQ651MNZ/Collage-de-Fotos-Jugueteria-Colorido-Blanco.png',
+    nombre: 'Diseños de publicidad',
+    descripcion: 'Creo diseños que capturan la atención y comunican mensajes efectivos.'
+  },
+  {
+    url: 'https://i.postimg.cc/8kJ1jmkg/video.png',
+    nombre: 'Creación de video',
+    descripcion: 'Desde la planificación hasta la producción y edición.'
+  },
+  {
+    url: 'https://i.postimg.cc/k46g3kTD/app.jpg',
+    nombre: 'App de asistencia',
+    descripcion: 'Desarrolle una app de asistencia para agilizar dicho proceso.'
+  },
+  {
+    url: 'https://i.postimg.cc/wvMG424q/logica.jpg',
+    nombre: 'Ejercicios de logica',
+    descripcion: 'Desarrollo ejercicios para mejorar  mis habilidades analíticas y de resolución de problemas.'
+  }
+];
 
-/* Funcion para hacer el recorrido automatico */
-function inicio(interval) {
-   stop(); // Detiene cualquier autoplay anterior para evitar múltiples intervalos.
-    navegacion = setInterval(() => {
-    navegar(1); // Navega al siguiente item cada intervalo de tiempo.
-    }, interval);
+let atras = document.getElementById("atras");
+let adelante = document.getElementById("adelante");
+let imagen = document.getElementById("img");
+let puntos = document.getElementById("puntos");
+let texto = document.getElementById("texto");
+let actual = 0;
+
+atras.addEventListener("click", function () {
+  actual -= 1
+  if (actual == -1) {
+    actual = imagenes.length - 1
+  }
+  imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="">`
+  texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+  posicionCarrucel()
+})
+adelante.addEventListener("click", function () {
+  actual += 1
+  if (actual == imagenes.length) {
+    actual = 0
+  }
+  imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="">`
+  texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+  posicionCarrucel()
+})
+
+function posicionCarrucel() {
+  puntos.innerHTML = "";
+  for (let i = 0; i < imagenes.length; i++) {
+    if (i == actual) {
+      puntos.innerHTML += '<p class="bold">.</p>'
+    } else {
+      puntos.innerHTML += '<p>.</p>'
+    }
+  }
 }
-function stop() {
-   clearInterval(automatico);
-}
-// Iniciar autoplay con un intervalo de 3 segundos.
-inicio(3000);
